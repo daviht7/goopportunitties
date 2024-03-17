95ab8c9a6e326df514f85dd27515d1858bf2dbe9
davi holanda <daviht7@gmail.com>
2024-03-17 15:47:35 -0300

    add logger customized

---
+++ config/config.go
@@ -0,0 +1,24 @@
+package config
+
+import (
+	"errors"
+
+	"gorm.io/gorm"
+)
+
+var (
+	db     *gorm.DB
+	logger *Logger
+)
+
+func Init() error {
+
+	return errors.New("fake error")
+
+}
+
+func GetLogger(p string) *Logger {
+	// Initilize Logger
+	logger = NewLogger(p)
+	return logger
+}
--- main.go
+++ main.go
@@ -1,11 +1,27 @@
 package main
 
-import "github.com/daviht7/goopportunitties/router"
+import (
+	"github.com/daviht7/goopportunitties/config"
+	"github.com/daviht7/goopportunitties/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
 
-	//Iniciando as routas
+	logger = config.GetLogger("main")
 
+	//Initialize Configs
+	err := config.Init()
+
+	if err != nil {
+		logger.Error(err)
+		return
+	}
+
+	//Iniciando as routas
 	router.Initialize()
 
 }
